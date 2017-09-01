a94ef66eb5a57e4f290b1bb1d007576f6d955f47
Long Wang <wanglong06@gmail.com>
2017-08-31 23:08:03 -0500

    finished resolved rates, force admittance, cleaned up force control options as well as resolved rates config

--- continuous_palpation.py
+++ continuous_palpation.py
@@ -15,27 +15,28 @@ from time import time
 # DEBUG TOOLS
 import ipdb
 
-def npToKdlFrame(npFrame):
-	quat = transformations.quaternion_from_matrix(npFrame)
-	rot = PyKDL.Rotation.Quaternion(quat[0],quat[1],quat[2],quat[3])
-	pos = PyKDL.Vector(npFrame[0,3],npFrame[1,3],npFrame[2,3])
-	kdlFrame = PyKDL.Frame.Identity()
-	kdlFrame.M = rot
-	kdlFrame.p = pos
-	return kdlFrame
+## The following is needed if we want to rely on the conversion.
+# def npToKdlFrame(npFrame):
+# 	quat = transformations.quaternion_from_matrix(npFrame)
+# 	rot = PyKDL.Rotation.Quaternion(quat[0],quat[1],quat[2],quat[3])
+# 	pos = PyKDL.Vector(npFrame[0,3],npFrame[1,3],npFrame[2,3])
+# 	kdlFrame = PyKDL.Frame.Identity()
+# 	kdlFrame.M = rot
+# 	kdlFrame.p = pos
+# 	return kdlFrame
 
-def kdlToNpFrame(kdlFrame):
-	npFrame = np.empty((4,4));
-	v = kdlFrame.M.UnitX()
-	npFrame[0,0:3] = [v.x(), v.y(), v.z()]
-	v = kdlFrame.M.UnitY()
-	npFrame[1,0:3] = [v.x(), v.y(), v.z()]
-	v = kdlFrame.M.UnitZ()
-	npFrame[2,0:3] = [v.x(), v.y(), v.z()]
-	npFrame[0:3,3] = [kdlFrame.p.x(),kdlFrame.p.y(),kdlFrame.p.z()]
-	npFrame[0:3,0:3] = npFrame[0:3,0:3] / np.linalg.norm(npFrame[0:3,0:3], axis=-1)[:, np.newaxis]
-	npFrame[3,:] = [0, 0, 0, 1]
-	return npFrame;
+# def kdlToNpFrame(kdlFrame):
+# 	npFrame = np.empty((4,4));
+# 	v = kdlFrame.M.UnitX()
+# 	npFrame[0,0:3] = [v.x(), v.y(), v.z()]
+# 	v = kdlFrame.M.UnitY()
+# 	npFrame[1,0:3] = [v.x(), v.y(), v.z()]
+# 	v = kdlFrame.M.UnitZ()
+# 	npFrame[2,0:3] = [v.x(), v.y(), v.z()]
+# 	npFrame[0:3,3] = [kdlFrame.p.x(),kdlFrame.p.y(),kdlFrame.p.z()]
+# 	npFrame[0:3,0:3] = npFrame[0:3,0:3] / np.linalg.norm(npFrame[0:3,0:3], axis=-1)[:, np.newaxis]
+# 	npFrame[3,:] = [0, 0, 0, 1]
+# 	return npFrame;
 
 class ContinuousPalpation:
     def __init__(self, psmName, forceTopic, bufferSize = 50):
@@ -70,23 +71,43 @@ class ContinuousPalpation:
 
         # TODO make these values not hard coded
         self.rate = rospy.Rate(1000) # 1000hz
-        self.fBias = np.array((0, 0, 1)) # Newtons
-        self.period = .5 # Seconds
-        self.amplitude = .5 # Newtons
-
+        self.forceProfile = \
+        {   'period':0.5, # Seconds
+            'amplitude': 0.5, # Newtons
+            'fBiasMag': 0.7, # Newtons, biased force magnitude
+            'magnitudeMode': 'bias', # 'bias' or 'sine', or 'sine bias'
+            'controlDir':'default', # 'default' or 'surf normal'
+            'defaultDir':[0.0,0.0,1.0],
+            'admittanceGains':[ 100.0/1000,\
+                                100.0/1000,\
+                                100.0/1000] # force admittance gains, (meter/sec)/Newton
+        }
+        self.resolvedRatesConfig = \
+        {   'velMin': 5.0/1000,
+            'velMax': 50.0/1000,
+            'angVelMin': 3.0/180.0*3.14,
+            'angVelMax': 60.0/180.0*3.14,
+            'tolPos': 0.5/1000, # positional tolerance
+            'tolRot': 1.0/180*3.14, # rotational tolerance
+            'velRatio': 10.0, # the ratio of max velocity error radius to tolarance radius, this value >1
+            'rotRatio': 5.0,
+            'dt': 1.0/1000, # this is the time step of the system. 
+                            # if rate=1khz, then dt=1.0/1000. However, 
+                            # we don't know if the reality will be the same as desired rate
+        }
         self.run()
 
     def run(self):
         nextPose = np.empty((3))
         while not rospy.is_shutdown():
             try:
+                ipdb.set_trace()
                 nextPose = self.trajectory[0]
             except IndexError:
                 # If no trajectory do nothing
                 continue
-            currentPose = self.robot.get_current_position()
-            xDotMotion = self.resolvedRates(currentPose, nextPose)
-            xDotForce = self.forceAdmittanceControl()
+            xDotMotion = self.resolvedRates() # xDotMotion is type [PyKDL.Twist]
+            xDotForce = self.forceAdmittanceControl() # xDotForce is type [PyKDL.Twist]
             xDot = hybridPosForce(xDotMotion,xDotForce)
             # self.robot.move(desiredPose, interpolate = False)
             self.trajStatusPub.publish(len(self.trajectory))
@@ -100,31 +121,99 @@ class ContinuousPalpation:
                                    data.wrench.force.y,
                                    data.wrench.force.z))
         self.f_buffer.append(f_current)
-
+    def averageForceBuffer(self):
+        # TODO - need to average the force buffer dequeue
+        f_average = PyKDL.Vector(0.0,0.0,0.0)
+        return f_average
     def poseCB(self, data):
-        self.trajectory.append( posemath.fromMsg(data.pose))
+        self.trajectory.append(posemath.fromMsg(data.pose))
     
     def poseArrayCB(self, data):
         for pose in data.poses:
-            self.trajectory.append( posemath.fromMsg(pose))
+            self.trajectory.append(posemath.fromMsg(pose))
 
-    def resolvedRates(self, poseCur, poseDes):
-        # TODO write resolved rates
-        xDotMotion = np.empty((6,1))
-        return xDotMotion
+    def resolvedRates(self):
+        # get current and desired robot pose (desired is the top of queue)
+        currentPose = self.robot.get_current_position()
+        desiredPose = self.trajectory[0]
+        # compute pose error (result in kdl.twist format)
+        poseError = PyKDL.diff(currentPose,desiredPose)
+        posErrNorm = poseError.vel.Norm()
+        rotErrNorm = poseError.rot.Norm()
+        # compute velocity magnitude based on position error norm
+        if posErrNorm>self.resolvedRatesConfig['tolPos']:
+            tolPosition = self.resolvedRatesConfig['tolPos']
+            lambdaVel = self.resolvedRatesConfig['velRatio']
+            if posErrNorm>(lambdaVel*tolPosition):
+                velMag = self.resolvedRatesConfig['velMax']
+            else:
+                velMax = self.resolvedRatesConfig['velMax']
+                velMin = self.resolvedRatesConfig['velMin']
+                velMag = velMin + \
+                (velMax - velMin)/(tolPosition*(lambdaVel-1))
+        else:
+            velMag = 0.0
+        # compute angular velocity based on rotation error norm
+        if rotErrNorm>self.resolvedRatesConfig['tolRot']:
+            tolRotation = self.resolvedRatesConfig['tolRot']
+            lambdaRot = self.resolvedRatesConfig['rotRatio']
+            if posErrNorm>(lambdaRot*tolRotation):
+                angVelMag = self.resolvedRatesConfig['angVelMax']
+            else:
+                angVelMax = self.resolvedRatesConfig['angVelMax']
+                angVelMin = self.resolvedRatesConfig['angVelMin']
+                angVelMag = angVelMin + \
+                (angVelMax - angVelMin)/(tolRotation*(lambdaRot-1))
+        else:
+            angVelMag = 0.0
+            # The resolved rates is implemented as Nabil Simaan's notes
+        # apply both the velocity and angular velocity in the error pose direction
+        sys_dt = self.resolvedRatesConfig['dt']
+        desiredTwist = PyKDL.Twist()
+        poseError.vel.Normalize() # normalize to have the velocity direction
+        desiredTwist.vel = poseError.vel*velMag*sys_dt
+        poseError.rot.Normalize() # normalize to have the ang vel direction
+        desiredTwist.rot = poseError.rot*angVelMag*sys_dt
+        return desiredTwist
 	
     def computeFRef(self):
-        # TODO compute fRef
-        mag = np.sin(self.period*time() / (2*np.PI)) * self.amplitude
-        displacement = (self.fBias / np.linalg.norm(self.fBias)) * mag
-        return self.fBias + displacement
+        if self.forceProfile['magnitudeMode']=='bias':
+            force_ref = self.forceProfile['fBiasMag']
+        elif self.forceProfile['magnitudeMode']=='sine':
+            force_ref = \
+            np.sin(self.forceProfile['period']*time() / (2*np.PI)) * \
+            self.forceProfile['amplitude']
+        else:
+            sine_mag = \
+            np.sin(self.forceProfile['period']*time() / (2*np.PI)) * \
+            self.forceProfile['amplitude']
+            force_ref = sine_mag + self.forceProfile['fBiasMag']
+        return force_ref
 
     def forceAdmittanceControl(self):
-        # TODO implement force admittance control
-        fRef = self.computeFRef()
-        xDotForce = np.empty((6,1))
-        # [xDotForce.p.x, xDotForce.p.x, xDotForce.p.y] = fRef;
-        return x_dot_motion
+        # load the desired force control direction according to different mode
+        if self.forceProfile['controlDir'] == 'default':
+            force_Ctrl_Dir = PyKDL.Vector(\
+                self.forceProfile['defaultDir'][0],
+                self.forceProfile['defaultDir'][1],
+                self.forceProfile['defaultDir'][2])
+        else: 
+            # under this condition, 
+            # need to compute the force control direction based on f_buffer
+            force_Ctrl_Dir = self.averageForceBuffer()
+        force_Ctrl_Dir.Normalize()
+        # compute the desired force magnitude  
+        f_ref_mag = self.computeFRef()
+        # compute desired force
+        desiredForce = force_Ctrl_Dir * f_ref_mag
+        # apply admittance gain to compute motion
+        sys_dt = self.resolvedRatesConfig['dt']
+        desiredTwist = PyKDL.Twist()
+        desiredTwist.vel = PyKDL.Vector(\
+            desiredForce.x()*self.forceProfile['admittanceGains'][0]*sys_dt,
+            desiredForce.y()*self.forceProfile['admittanceGains'][1]*sys_dt,
+            desiredForce.z()*self.forceProfile['admittanceGains'][2]*sys_dt) 
+        return desiredTwist
 
     def hybridPosForce(self, xDotMotion, xDotForce, poseCur):
         # TODO implement hybrid force position control
---
+++ pub_test_script.sh
@@ -0,0 +1,25 @@
+rostopic pub /set_continuous_palpation_trajectory geometry_msgs/PoseArray "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+poses:
+- position:
+    x: 0.01
+    y: 0.01
+    z: 0.01
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+- position:
+    x: 0.015
+    y: 0.015
+    z: 0.015
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0"
